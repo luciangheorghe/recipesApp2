9ef01a46f028427536a83fc85012b98d0cc3454f
Lucian Gheorghe <lucian.gheorghe1989@gmail.com>
2018-06-09 14:14:26 +0300

    app file update

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def recipes():
     ingredient = request.form.get('ingred')
     print(ingredient)
 
-    find_recipes = list(mongo.db.recipes.find({"time.total": ingredient}))
+    find_recipes = mongo.db.recipes.find({"time.total": ingredient})
     print(find_recipes)
 
     if int(find_recipes) > 0:
